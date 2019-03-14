8b7b1fae742ffbb9699856a1a9672af167dafd1d
Andrew Holguin <ajholguin1@gmail.com>
2019-03-14 16:31:06 -0700

    add basic read_cmep_chunked func

--- R/read.R
+++ R/read.R
@@ -1,11 +1,16 @@
 #' Read CMEP File
 #'
-#' Read and parse CMEP file. Currently, the functions are just simple wrappers
-#' around the more generic \code{readr} functions.
+#' Read and parse CMEP file. These functions are mostly just simple wrappers
+#' around the more generic \code{readr} functions. The main enhancement is that
+#' they will parse an irregularly sized CMEP file and format the data as a
+#' regular table (data frame). \code{read_cmep_chunked} can be used to process
+#' files that may be too large to fit in memory.
 #'
-#' @param ... passed to \code{\link[readr]{read_lines}}
+#' @param ... passed to \code{\link[readr]{read_lines}} or
+#'   \code{\link[readr]{read_lines_chunked}}
 #' @param parse boolean; should the records be parsed?
 #'
+#' @rdname read_cmep
 #' @export
 #' @examples
 #' f <- system.file("extdata", "cmep.dat", package = "cmep", mustWork = TRUE)
@@ -14,4 +19,23 @@ read_cmep <- function(..., parse = TRUE) {
   x <- readr::read_lines(...)
   if (parse) x <- parse_cmep(x)
   x
+}
+
+#' @rdname read_cmep
+#' @export
+#' @examples
+#' f <- system.file("extdata", "cmep.dat", package = "cmep", mustWork = TRUE)
+#' cb_f <- function(x, pos) print(pos)
+#' read_cmep_chunked(f, callback = cb_f, chunk_size = 2)
+read_cmep_chunked <- function(..., parse = TRUE) {
+  dots <- list(...)
+  if (parse) {
+    # call `parse_cmep` on x before the normal callback function
+    cbf <- dots$callback
+    dots$callback <- function(x, pos) {
+      x <- parse_cmep(x)
+      do.call(cbf, list(x, pos))
+    }
+  }
+  do.call(readr::read_lines_chunked, dots)
 }
