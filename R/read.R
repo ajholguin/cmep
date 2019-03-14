928af8bc2e88fb84cf67516c91d6c094e3d3a033
Andrew Holguin <ajholguin1@gmail.com>
2019-03-14 16:54:07 -0700

    add unnest as argument to the read functions

--- R/read.R
+++ R/read.R
@@ -9,15 +9,17 @@
 #' @param ... passed to \code{\link[readr]{read_lines}} or
 #'   \code{\link[readr]{read_lines_chunked}}
 #' @param parse boolean; should the records be parsed?
+#' @param unnest boolean; should the data values be unnested? if FALSE (the
+#'   default) then they are returned in a list column with nested tables
 #'
 #' @rdname read_cmep
 #' @export
 #' @examples
 #' f <- system.file("extdata", "cmep.dat", package = "cmep", mustWork = TRUE)
 #' read_cmep(f)
-read_cmep <- function(..., parse = TRUE) {
+read_cmep <- function(..., parse = TRUE, unnest = FALSE) {
   x <- readr::read_lines(...)
-  if (parse) x <- parse_cmep(x)
+  if (parse) x <- parse_cmep(x, unnest)
   x
 }
 
@@ -27,13 +29,13 @@ read_cmep <- function(..., parse = TRUE) {
 #' f <- system.file("extdata", "cmep.dat", package = "cmep", mustWork = TRUE)
 #' cb_f <- function(x, pos) print(pos)
 #' read_cmep_chunked(f, callback = cb_f, chunk_size = 2)
-read_cmep_chunked <- function(..., parse = TRUE) {
+read_cmep_chunked <- function(..., parse = TRUE, unnest = FALSE) {
   dots <- list(...)
   if (parse) {
     # call `parse_cmep` on x before the normal callback function
     cbf <- dots$callback
     dots$callback <- function(x, pos) {
-      x <- parse_cmep(x)
+      x <- parse_cmep(x, unnest)
       do.call(cbf, list(x, pos))
     }
   }
